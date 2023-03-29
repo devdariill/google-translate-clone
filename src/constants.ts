8484059b3747938f5b814e8dbe97a79402b7a2d7
nATOr <gowluonade@gmail.com>
2023-03-29 14:52:13 -0500

    _types

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,13 @@
 export const SUPPORTED_LANGUAGES = {
   en: 'English',
-  es: 'Spanish',
+  es: 'Español',
   de: 'Deutsch'
+}
+
+export const VOICE_FOR_LANGUAGE = {
+  en: 'en-GB',
+  es: 'es-MX',
+  de: 'de-DE'
 }
 
 export const AUTO_LANGUAGE = 'auto'
