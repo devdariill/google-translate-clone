343483f1d8a666771fe051bbe9b41de1f914bb6a
nATOr <gowluonade@gmail.com>
2023-03-29 16:12:24 -0500

    Multiple Types

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,7 +1,13 @@
 import { Form } from 'react-bootstrap'
 import { SUPPORTED_LANGUAGES } from '../constants'
+import { type FC } from 'react'
 
-export const LanguageSelector = ({ onChange }) => {
+interface Props {
+  onChange: (language: string) => void
+}
+// export const LanguageSelector = ({ onChange }: { onChange: (language: string) => void }) => {
+// export const LanguageSelector = ({ onChange }: Props) => {
+export const LanguageSelector: FC<Props> = ({ onChange }) => {
   return (
         <Form.Select aria-label="Selecciona un idioma">
             {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
