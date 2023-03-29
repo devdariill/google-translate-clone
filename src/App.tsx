5440f927fe173aff2a36478a89e4af0cadb1098f
nATOr <gowluonade@gmail.com>
2023-03-29 13:28:06 -0500

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromTexto: '',
+  result: '',
+  loading: false
+}
 
+function reducer (state, action) {
+  const { type, payload } = action
+  if (type === 'INTERCHANGE_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loadin: true,
+      fromTexto: payload,
+      result: ''
+    }
+  }
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loadin: false,
+      result: payload
+    }
+  }
+  return state
+}
+
+function App () {
+  const [state, setState] = useState(reducer, initialState)
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+ <main>
+  <h1>Google Translate</h1>
+ </main>
   )
 }
 
