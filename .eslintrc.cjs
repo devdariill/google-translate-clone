e91401bd7de9425d0f5199d29fd4dc1b9c5015d9
nATOr <gowluonade@gmail.com>
2023-03-29 18:22:24 -0500

    _r

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    '@typescript-eslint/key-spacing': 'off'
   }
 }
