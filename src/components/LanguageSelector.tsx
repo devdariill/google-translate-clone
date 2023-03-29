e2d6a164e0046d5292a9843af3b72e02239e2972
nATOr <gowluonade@gmail.com>
2023-03-29 16:16:44 -0500

    habdleChange-React.ChangeEvent<HTMLSelectElement>

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -8,8 +8,11 @@ interface Props {
 // export const LanguageSelector = ({ onChange }: { onChange: (language: string) => void }) => {
 // export const LanguageSelector = ({ onChange }: Props) => {
 export const LanguageSelector: FC<Props> = ({ onChange }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(event.target.value)
+  }
   return (
-        <Form.Select aria-label="Selecciona un idioma">
+        <Form.Select aria-label="Selecciona un idioma" onChange={handleChange}>
             {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
                 <option key={key} value={literal}>{literal}</option>
             ))}
