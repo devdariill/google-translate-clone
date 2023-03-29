e604e3582b69d64a0911c39ab6c0c2c2dd5d17ed
nATOr <gowluonade@gmail.com>
2023-03-29 16:41:53 -0500

    _t

--- src/types.d.ts
+++ src/types.d.ts
@@ -18,8 +18,3 @@ export type Action =
   | { type: 'SET_TO_LANGUAGE', payload: Language }
   | { type: 'SET_FROM_TEXT', payload: string }
   | { type: 'SET_RESULT', payload: string }
-
-export enum SectionType {
-  From = 'from',
-  To = 'to'
-}
