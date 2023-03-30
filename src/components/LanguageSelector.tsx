03bf76e0dba251b68eebb2c65bb31d318a350b86
nATOr <gowluonade@gmail.com>
2023-03-29 19:53:43 -0500

    _

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,28 +1,29 @@
 import { Form } from 'react-bootstrap'
 import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from '../constants'
-import { type FC } from 'react'
 import { SectionType, type FromLanguage, type Language } from '../types.d'
 
 // interface Props {
 //   onChange: (language: Language) => void
 // }
 type Props =
-| { type: SectionType.From, value: FromLanguage, onChange: (language: FromLanguage) => void }
-| { type: SectionType.To, value: Language, onChange: (language: Language) => void }
+  | { type: SectionType.From, value: FromLanguage, onChange: (language: FromLanguage) => void }
+  | { type: SectionType.To, value: Language, onChange: (language: Language) => void }
 // export const LanguageSelector = ({ onChange }: { onChange: (language: string) => void }) => {
 // export const LanguageSelector = ({ onChange }: Props) => {
-export const LanguageSelector: FC<Props> = ({ onChange, type, value }) => {
+export const LanguageSelector = ({ onChange, type, value }: Props) => {
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     onChange(event.target.value as Language)
   }
+
   return (
-    <Form.Select aria-label="Selecciona un idioma" onChange={handleChange} value={value}>
-      {type === SectionType.From && <option value={AUTO_LANGUAGE}>Detectar idioma</option>}
-      {
-        Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
-          <option key={key} value={literal}>{literal}</option>
-        ))
-      }
-    </Form.Select>
+      <Form.Select aria-label='Selecciona el idioma' onChange={handleChange} value={value}>
+        {type === SectionType.From && <option value={AUTO_LANGUAGE}>Detectar idioma</option>}
+
+        {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
+          <option key={key} value={key}>
+            {literal}
+          </option>
+        ))}
+      </Form.Select>
   )
 }
