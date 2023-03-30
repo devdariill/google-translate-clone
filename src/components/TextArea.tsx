e4b932f9cb5d8e489e12ef1bb1af1b3457c5b1d4
nATOr <gowluonade@gmail.com>
2023-03-29 19:54:23 -0500

    _

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -4,32 +4,36 @@ import { SectionType } from '../types.d'
 interface Props {
   type: SectionType
   loading?: boolean
-  onChange: (language: string) => void
+  onChange: (value: string) => void
   value: string
 }
 
-const commonStyles = { height: '200px', border: 0, resize: 'none' }
+const commonStyles = { border: 0, height: '200px', resize: 'none' }
+
 const getPlaceholder = ({ type, loading }: { type: SectionType, loading?: boolean }) => {
-  if (type === SectionType.From) return 'Escribe algo'
+  if (type === SectionType.From) return 'Introducir texto'
   if (loading === true) return 'Cargando...'
   return 'Traducción'
 }
-function TextArea ({ loading, type, value, onChange }: Props) {
+
+export const TextArea = ({ type, loading, value, onChange }: Props) => {
   const styles = type === SectionType.From
     ? commonStyles
     : { ...commonStyles, backgroundColor: '#f5f5f5' }
+
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     onChange(event.target.value)
   }
+
   return (
     <Form.Control
-    autoFocus={type === SectionType.From}
-    as="textarea"
-    disabled={type === SectionType.To}
-    placeholder={getPlaceholder({ type, loading })}
-    style={styles}
-    value={value}
-    onChange={handleChange} />
+      autoFocus={type === SectionType.From}
+      as='textarea'
+      disabled={type === SectionType.To}
+      placeholder={getPlaceholder({ type, loading })}
+      style={styles}
+      value={value}
+      onChange={handleChange}
+    />
   )
 }
-export default TextArea
